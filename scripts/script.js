c95e73f0a3cca828587e638629056fb25e76b246
Yasya <80125891+yasya-ayesha@users.noreply.github.com>
2021-07-07 14:01:02 +0300

    Add files via upload
    realised filling by goods from json base and added filtration by categories

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+let hash = location.hash.substring(1);
+
+
 const headerCityButton = document.querySelector('.header__city-button');
 
 headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
@@ -33,7 +36,7 @@ const enableScroll = () => {
     });
 };
 
-// модальное окно
+// модальное окно Корзина - переменные
 
 const subheaderCart = document.querySelector('.subheader__cart');
 const cartOverlay = document.querySelector('.cart-overlay');
@@ -48,6 +51,35 @@ const cartModalClose = () => {
     enableScroll();
 };
 
+// запрос базы данных
+
+const getData = async () => {
+    const data = await fetch('db.json');
+
+    if (data.ok) {
+        return data.json();
+    } else {
+        throw new Error(`Данные не были получены, ошибка ${data.status} ${data.statusText}`);
+    }
+};
+
+const getGoods = (callback, value) => {
+    getData()
+        .then(data => {
+            if (value) {
+                callback(data.filter(item => item.category === value));
+            } else {
+                callback(data);
+            }
+            
+        })
+        .catch(err => {
+            console.error(err);
+        });
+};
+
+// модальное окно Корзина - вызов функции
+
 subheaderCart.addEventListener('click', cartModalOpen);
 
 cartOverlay.addEventListener('click', event => {
@@ -56,14 +88,95 @@ cartOverlay.addEventListener('click', event => {
     if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
         cartModalClose();
     }
-    
 });
 
 document.addEventListener('keydown', event => {
 
-    if (event.keyCode === 27) { 
+    if (event.code === 'Escape') { 
         cartModalClose();
     }
 });
+
+// фильтрация по категориям
+
+function localTitle() {
+    const goodsTitle = document.querySelector('.goods__title');
+
+    if (hash === 'men') {
+        goodsTitle.textContent = 'Мужчинам';
+    }
+    if (hash === 'women') {
+        goodsTitle.textContent = 'Женщинам';
+    }
+    if (hash === 'kids') {
+        goodsTitle.textContent = 'Детям';
+    }
+}
+
+localTitle();
+
+try {
+
+    const goodsList = document.querySelector('.goods__list');
+
+    if (!goodsList) {
+        throw 'This is not a goods page!';
+    }
+
+    const createCard = ({ id, preview, cost, brand, name, sizes }) => {
+
+        const li = document.createElement('li');
+        li.classList.add('goods__item');
+
+        li.innerHTML = `
+            <article class="good">
+                <a class="good__link-img" href="card-good.html#${id}">
+                    <img class="good__img" src="goods-image/${preview}" alt="">
+                </a>
+                <div class="good__description">
+                    <p class="good__price">${cost} &#8381;</p>
+                    <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+                    ${sizes ? 
+                        `<p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(' ')}</span></p>` : 
+                        ''}
+                    <a class="good__link" href="card-good.html#${id}">Подробнее</a>
+                </div>
+            </article>
+        `;
+
+        return li;
+    };
+
+    const renderGoodsList = data => {
+        goodsList.textContent = ''; // - вставляет пустую строку
+        // goodsList.innerHTML = ''; - другой способ, обнуляет html
+
+        // for (let i = 0; i < data.length; i++) {
+        //     console.log('for:', data[i]);
+        // } - способ перебрать массив
+
+        // for (const item of data) {
+        //     console.log('for/of:', item);
+        // } - другой способ перебрать массив
+
+        data.forEach((item) => {
+            const card = createCard(item);
+            goodsList.append(card);
+        });
+    };
+    
+    window.addEventListener('hashchange', () => {
+        hash = location.hash.substring(1);
+        getGoods(renderGoodsList, hash);
+        
+        localTitle();
+    });
+
+    getGoods(renderGoodsList, hash);
+
+} catch (err) {
+    console.warn(err);
+}
+
 
     
